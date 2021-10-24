d1d467328c2d127030d86bd30e3d30b6eff8d92c
samansupriadi <samansenju@gmail.com>
2021-10-24 22:56:05 +0700

    array sort callback

--- sesi1/array.js
+++ sesi1/array.js
@@ -1,5 +1,19 @@
-let arr = [
+  let arr = [
 	1, "dua", true, ["array lagi"]
 ]
 
-console.log(arr)
+//console.log(arr)
+
+arr.push("wahyu")
+arr.sort()
+//console.log(arr)
+
+
+let arr2 = [1, 2, 3, 4, 10, 14, 20, 16]
+function compare(a, b){
+ console.log(a, b)
+ return a - b
+}
+arr2.sort(compare)
+
+console.log(arr2)
